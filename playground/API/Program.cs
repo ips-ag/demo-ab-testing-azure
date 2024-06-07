634bdf8787fe200f042de701307efbcf4f7f93de
Steve Tran <stevetran.dev@gmail.com>
2024-06-07 14:05:49 +0700

    chore: Update package.json and .dockerignore files

--- playground/API/Program.cs
+++ playground/API/Program.cs
@@ -21,16 +21,16 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<EcommerceContext>(opt =>
 {
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+  opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddDbContext<ApplicationIdentityDbContext>(opt =>
 {
-    opt.UseSqlite(builder.Configuration.GetConnectionString("IdentityConnection"));
+  opt.UseSqlite(builder.Configuration.GetConnectionString("IdentityConnection"));
 });
 builder.Services.AddSingleton<IConnectionMultiplexer>(c =>
 {
-    var config = ConfigurationOptions.Parse(builder.Configuration.GetConnectionString("Redis"), true);
-    return ConnectionMultiplexer.Connect(config);
+  var config = ConfigurationOptions.Parse(builder.Configuration.GetConnectionString("Redis"), true);
+  return ConnectionMultiplexer.Connect(config);
 });
 builder.Services.Configure<TokenSettings>(builder.Configuration.GetSection("TokenSettings"));
 
@@ -46,40 +46,43 @@ builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
 
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowAngularApp", builder =>
-    {
-        builder.WithOrigins("http://localhost:4200") // Replace with the actual Angular app URL.
-            .AllowAnyHeader()
-            .AllowAnyMethod();
-    });
+  options.AddPolicy("AllowAngularApp", builder =>
+  {
+    builder.WithOrigins("http://localhost:4200") // Replace with the actual Angular app URL.
+          .AllowAnyHeader()
+          .AllowAnyMethod();
+  });
 });
 // Configure authentication and authorization middleware
+var tokenSettings = builder.Configuration.GetSection("TokenSettings").Get<TokenSettings>();
+var fileProvider = "appsettings.json";
+var fileInfo = builder.Environment.ContentRootFileProvider.GetFileInfo(fileProvider);
+Console.WriteLine("Using appsettings.json file at: " + fileInfo.PhysicalPath);
 builder.Services.AddAuthentication(options =>
 {
-    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+  options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+  options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+  options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
 }).AddJwtBearer(options =>
 {
-    var tokenSettings = builder.Configuration.GetSection("TokenSettings").Get<TokenSettings>();
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidateLifetime = true,
-        ValidateIssuerSigningKey = true,
-        ValidIssuer = tokenSettings.Issuer,
-        ValidAudience = tokenSettings.Audience,
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenSettings.Key))
-    };
+  options.TokenValidationParameters = new TokenValidationParameters
+  {
+    ValidateIssuer = true,
+    ValidateAudience = true,
+    ValidateLifetime = true,
+    ValidateIssuerSigningKey = true,
+    ValidIssuer = tokenSettings.Issuer,
+    ValidAudience = tokenSettings.Audience,
+    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenSettings.Key))
+  };
 });
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+  app.UseSwagger();
+  app.UseSwaggerUI();
 }
 
 app.UseCors("AllowAngularApp");
@@ -98,15 +101,15 @@ var logger = loggerFactory.CreateLogger<EcommerceContextSeed>();
 
 try
 {
-    await context.Database.MigrateAsync();
-    // Create an instance of EcommerceContextSeed
-    var ecommerceContextSeed = new EcommerceContextSeed(logger);
-    await ecommerceContextSeed.SeedDataAsync(context);
-    // Seed data for ApplicationIdentityDbContext
-    await ApplicationIdentityDbContextSeed.SeedAsync(services);
+  await context.Database.MigrateAsync();
+  // Create an instance of EcommerceContextSeed
+  var ecommerceContextSeed = new EcommerceContextSeed(logger);
+  await ecommerceContextSeed.SeedDataAsync(context);
+  // Seed data for ApplicationIdentityDbContext
+  await ApplicationIdentityDbContextSeed.SeedAsync(services);
 }
 catch (Exception ex)
 {
-    logger.LogError(ex, "An error occured during migration");
+  logger.LogError(ex, "An error occured during migration");
 }
 app.Run();
