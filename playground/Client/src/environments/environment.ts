c69626fb307cc572bb9a2a7e398757d3962861bc
Quang Tran Anh <aquangtran@win.ips-ag.net>
2024-06-18 18:03:44 +0700

    update clarity env

--- playground/Client/src/environments/environment.ts
+++ playground/Client/src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
   analytics: {
-    clarityId: 'mq8lp34s8t',
-    googleAnalyticsMeasurementId: 'G-CJLP5B10EW',
+    clarityId: 'mq5tfbapai',
+    googleAnalyticsMeasurementId: 'G-1YBSRMHGPJ',
   },
   baseUrl: ''
 };
