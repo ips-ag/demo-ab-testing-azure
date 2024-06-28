905c8735dde3a6514575a407afcb7ada93931f0d
it.luudinhuyen@gmail.com <it.luudinhuyen@gmail.com>
2024-06-28 11:59:46 +0700

    test yml

--- modules/GetBuildVersion.psm1
+++ modules/GetBuildVersion.psm1
@@ -3,32 +3,31 @@ Function GetBuildVersion {
         [string]$VersionString
     )
 
-    # Initialize default values
-    $Major = 0
-    $Minor = 0
-    $Patch = 0
-    $PreReleaseTag = ""
-    $BuildRevision = 0
-
     # Process through regex
-    if ($VersionString -match "^(?<major>\d+)(\.(?<minor>\d+))?(\.(?<patch>\d+))?(\-(?<pre>[0-9A-Za-z\-\.]+))?(\+(?<build>\d+))?$") {
-        if ($matches['major']) { $Major = [uint64]$matches['major'] }
-        if ($matches['minor']) { $Minor = [uint64]$matches['minor'] }
-        if ($matches['patch']) { $Patch = [uint64]$matches['patch'] }
-        if ($matches['pre']) { $PreReleaseTag = [string]$matches['pre'] }
-        if ($matches['build']) { $BuildRevision = [uint64]$matches['build'] }
-    } else {
+    $VersionString -match "(?<major>\d+)(\.(?<minor>\d+))?(\.(?<patch>\d+))?(\-(?<pre>[0-9A-Za-z\-\.]+))?(\+(?<build>\d+))?" | Out-Null
+
+    if ($matches -eq $null) {
         return "1.0.0-build"
     }
 
-    # Construct the version string
-    $Version = [string]$Major + '.' + [string]$Minor + '.' + [string]$Patch
+    # Extract the build metadata
+    $BuildRevision = [uint64]$matches['build']
+    # Extract the pre-release tag
+    $PreReleaseTag = [string]$matches['pre']
+    # Extract the patch
+    $Patch = [uint64]$matches['patch']
+    # Extract the minor
+    $Minor = [uint64]$matches['minor']
+    # Extract the major
+    $Major = [uint64]$matches['major']
+
+    $Version = [string]$Major + '.' + [string]$Minor + '.' + [string]$Patch;
     if ($PreReleaseTag -ne [string]::Empty) {
         $Version = $Version + '-' + $PreReleaseTag
     }
 
     if ($BuildRevision -ne 0) {
-        $Version = $Version + '+' + [string]$BuildRevision
+        $Version = $Version + '.' + [string]$BuildRevision
     }
 
     return $Version
