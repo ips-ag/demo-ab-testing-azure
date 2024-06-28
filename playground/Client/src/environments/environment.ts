c293d43f2fe275f1f8d5787be4628f25d3667cbe
it.luudinhuyen@gmail.com <it.luudinhuyen@gmail.com>
2024-06-28 10:36:37 +0700

    Update login message

--- playground/Client/src/environments/environment.ts
+++ playground/Client/src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
   analytics: {
-    clarityId: 'mq5tfbapai',
-    googleAnalyticsMeasurementId:  'G-1YBSRMHGPJ',
+    clarityId: process.env.ANALYTICS_CLARITY_ID, // 'mq5tfbapai',
+    googleAnalyticsMeasurementId: process.env.ANALYTICS_GA_MEASUREMENT_ID, // 'G-1YBSRMHGPJ',
   },
   baseUrl: '',
 };
