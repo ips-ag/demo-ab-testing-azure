2315ff7ef944cd6149e34eb0a23a19daf1acb2e3
it.luudinhuyen@gmail.com <it.luudinhuyen@gmail.com>
2024-06-28 11:29:49 +0700

    Update GetBuildVersion.psm1

--- modules/GetBuildVersion.psm1
+++ modules/GetBuildVersion.psm1
@@ -3,31 +3,32 @@ Function GetBuildVersion {
         [string]$VersionString
     )
 
-    # Process through regex
-    $VersionString -match "(?<major>\d+)(\.(?<minor>\d+))?(\.(?<patch>\d+))?(\-(?<pre>[0-9A-Za-z\-\.]+))?(\+(?<build>\d+))?" | Out-Null
+    # Initialize default values
+    $Major = 0
+    $Minor = 0
+    $Patch = 0
+    $PreReleaseTag = ""
+    $BuildRevision = 0
 
-    if ($matches -eq $null) {
+    # Process through regex
+    if ($VersionString -match "^(?<major>\d+)(\.(?<minor>\d+))?(\.(?<patch>\d+))?(\-(?<pre>[0-9A-Za-z\-\.]+))?(\+(?<build>\d+))?$") {
+        if ($matches['major']) { $Major = [uint64]$matches['major'] }
+        if ($matches['minor']) { $Minor = [uint64]$matches['minor'] }
+        if ($matches['patch']) { $Patch = [uint64]$matches['patch'] }
+        if ($matches['pre']) { $PreReleaseTag = [string]$matches['pre'] }
+        if ($matches['build']) { $BuildRevision = [uint64]$matches['build'] }
+    } else {
         return "1.0.0-build"
     }
 
-    # Extract the build metadata
-    $BuildRevision = [uint64]$matches['build']
-    # Extract the pre-release tag
-    $PreReleaseTag = [string]$matches['pre']
-    # Extract the patch
-    $Patch = [uint64]$matches['patch']
-    # Extract the minor
-    $Minor = [uint64]$matches['minor']
-    # Extract the major
-    $Major = [uint64]$matches['major']
-
-    $Version = [string]$Major + '.' + [string]$Minor + '.' + [string]$Patch;
+    # Construct the version string
+    $Version = [string]$Major + '.' + [string]$Minor + '.' + [string]$Patch
     if ($PreReleaseTag -ne [string]::Empty) {
         $Version = $Version + '-' + $PreReleaseTag
     }
 
     if ($BuildRevision -ne 0) {
-        $Version = $Version + '.' + [string]$BuildRevision
+        $Version = $Version + '+' + [string]$BuildRevision
     }
 
     return $Version
