5aa01dba19f255634f5fbfafed0623c19e39e752
Uyen Luu <uyen.dinhluu@ips-ag.com>
2024-07-09 09:55:42 +0700

    Avoid using dynamic type, especially as API model. Probably the best option is to replace it with string

--- packages/Nuget/IPSAG.AbTesting/Services/ConfigurationService.cs
+++ packages/Nuget/IPSAG.AbTesting/Services/ConfigurationService.cs
@@ -5,19 +5,19 @@ namespace IPSAG.AbTesting.Services;
 
 public interface IConfigurationService
 {
-    Task<IReadOnlyDictionary<string, dynamic>> ReadFeatureFlagsAsync(CancellationToken ct = default);
+    Task<IReadOnlyDictionary<string, string?>> ReadFeatureFlagsAsync(CancellationToken ct = default);
 }
 
 internal class ConfigurationService(IVariantFeatureManagerSnapshot featureManager) : IConfigurationService
 {
-    public async Task<IReadOnlyDictionary<string, dynamic>> ReadFeatureFlagsAsync(CancellationToken ct = default)
+    public async Task<IReadOnlyDictionary<string, string?>> ReadFeatureFlagsAsync(CancellationToken ct = default)
     {
-        var featureFlags = new Dictionary<string, dynamic>();
+        var featureFlags = new Dictionary<string, string?>();
         var featureNames = featureManager.GetFeatureNamesAsync(ct).ToBlockingEnumerable(ct);
         foreach (var featureName in featureNames)
         {
             var variant = await featureManager.GetVariantAsync(featureName, ct).ConfigureAwait(false);
-            featureFlags.Add(featureName, variant != null ? variant.Configuration.Get<dynamic>() : await featureManager.IsEnabledAsync(featureName, ct));
+            featureFlags.Add(featureName, variant != null ? variant.Configuration.Get<string?>() : (await featureManager.IsEnabledAsync(featureName, ct)).ToString());
         }
         return featureFlags;
     }
